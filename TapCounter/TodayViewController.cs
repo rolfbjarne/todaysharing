f808506b2aedcea833e12c719a124d08a4a6f8eb
Rolf Bjarne Kvinge <rolf@xamarin.com>
2014-10-09 11:28:03 -0400

    Today sharing extension.

---
+++ TapCounter/TodayViewController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Drawing;
+
+using NotificationCenter;
+using Foundation;
+using Social;
+using UIKit;
+
+namespace TapCounter
+{
+	public partial class TodayViewController : SLComposeServiceViewController, INCWidgetProviding
+	{
+		public TodayViewController (IntPtr handle) : base (handle)
+		{
+		}
+
+		public override void DidReceiveMemoryWarning ()
+		{
+			// Releases the view if it doesn't have a superview.
+			base.DidReceiveMemoryWarning ();
+
+			// Release any cached data, images, etc that aren't in use.
+		}
+
+		public override void ViewDidLoad ()
+		{
+			base.ViewDidLoad ();
+
+			// Do any additional setup after loading the view.
+			View.BackgroundColor = UIColor.LightGray;
+		}
+
+		public override void TouchesEnded (NSSet touches, UIEvent evt)
+		{
+			base.TouchesEnded (touches, evt);
+
+			var defs = new NSUserDefaults ("group.com.xamarin.todaysharing", NSUserDefaultsType.SuiteName);
+			if (defs.ValueForKey (new NSString ("key")) != null) {
+				defs.RemoveObject ("key");
+				View.BackgroundColor = UIColor.DarkGray;
+			} else {
+				defs.SetValueForKey (new NSString ("value"), new NSString ("key"));
+				View.BackgroundColor = UIColor.Green;
+			}
+			defs.Synchronize ();
+		}
+
+		public void WidgetPerformUpdate (Action<NCUpdateResult> completionHandler)
+		{
+			// Perform any setup necessary in order to update the view.
+
+			// If an error is encoutered, use NCUpdateResultFailed
+			// If there's no update required, use NCUpdateResultNoData
+			// If there's an update, use NCUpdateResultNewData
+
+			completionHandler (NCUpdateResult.NewData);
+		}
+	}
+}
+
---
+++ TodaySharing/AppDelegate.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using MonoTouch.Foundation;
+using MonoTouch.UIKit;
+
+namespace TodaySharing
+{
+	[Register ("AppDelegate")]
+	public partial class AppDelegate : UIApplicationDelegate
+	{
+		UIWindow window;
+
+		UIViewController vc;
+
+		public override bool FinishedLaunching (UIApplication app, NSDictionary options)
+		{
+			window = new UIWindow (UIScreen.MainScreen.Bounds);
+
+			vc = new UIViewController ();
+			vc.View.BackgroundColor = UIColor.Blue;
+			window.RootViewController = vc;
+
+			UIColor lastColor = UIColor.Green;
+			NSTimer.CreateRepeatingScheduledTimer (1, () => {
+				var defs = new NSUserDefaults ("group.com.xamarin.todaysharing", NSUserDefaultsType.SuiteName);
+				var value = defs.ValueForKey (new NSString ("key"));
+				if (value == null) {
+					if (lastColor == UIColor.Yellow)
+						lastColor = UIColor.Orange;
+					else 
+						lastColor = UIColor.Yellow;
+					vc.View.BackgroundColor = lastColor;
+				} else {
+					vc.View.BackgroundColor = UIColor.Green;
+				}
+			});
+
+			window.MakeKeyAndVisible ();
+			
+			return true;
+		}
+	}
+}
+
